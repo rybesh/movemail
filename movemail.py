8df9db494c45cca9a7adcc6a95dc6f0f1ef0ef52
Ryan Shaw <ryanshaw@unc.edu>
2015-10-21 08:43:59 -0400

    Remove filtering stuff.

--- movemail.py
+++ movemail.py
@@ -1,11 +1,8 @@
 #! /usr/bin/env python
 
-import xml.etree.ElementTree as etree
 import email
 import imaplib
 import smtplib
-import os
-import re
 import sys
 
 from secrets import (FROM_IMAP, FROM_USER, FROM_PASSWORD,
@@ -16,82 +13,6 @@ DEBUG = False
 if DEBUG:
     imaplib.Debug = 4
 
-ATOM_NS = 'http://www.w3.org/2005/Atom'
-APPS_NS = 'http://schemas.google.com/apps/2006'
-
-
-class Filter:
-    listre = re.compile(r'list:\((?P<listid>.+)\)')
-
-    def __init__(self, entry):
-        self.id = entry.find('{%s}id' % ATOM_NS).text
-        self.msg_ids = []
-        self.labels = []
-        self.forwardees = []
-        self.criteria = []
-        self.trash = self.star = self.archive \
-            = self.alwaysmarkasimportant = False
-        for prop in entry.findall('{%s}property' % APPS_NS):
-            name = prop.attrib['name'].lower()
-            value = prop.attrib['value']
-            if name.startswith('should'):
-                self.__dict__[name[6:]] = True
-            else:
-                handler = getattr(self, 'handle_%s' % name, None)
-                if handler is None:
-                    self.add_search_criteria(name, value)
-                else:
-                    handler.__call__(value)
-        self.criteria.extend(['NOT', 'FLAGGED'])
-        if self.trash:
-            self.labels.append('[Gmail]/Trash')
-        if self.alwaysmarkasimportant:
-            self.labels.append('[Gmail]/Important')
-        if self.star:
-            self.labels.append('[Gmail]/Starred')
-        self.error = None
-
-    def handle_hastheword(self, value):
-        match = self.listre.match(value)
-        if match:
-            self.criteria.extend(
-                ['HEADER', '"List-Id"', '"%s"' % match.group('listid')])
-
-    def handle_label(self, value):
-        self.labels.append(value)
-
-    def handle_smartlabeltoapply(self, value):
-        self.labels.append(value)
-
-    def handle_forwardto(self, value):
-        self.forwardees.append(value)
-
-    def add_search_criteria(self, name, value):
-        self.criteria.append('HEADER')
-        self.criteria.append(name.upper())
-        self.criteria.append('"%s"' % value)
-
-    def run(self, account, smtp=None):
-        try:
-            code, response = account.search(None, *self.criteria)
-            if code == 'OK':
-                self.msg_ids = response[0].split()
-                if DEBUG:
-                    print '%s\nMatched %s messages.' % (
-                        ' '.join(self.criteria), len(self.msg_ids))
-                if len(self.msg_ids) > 0:
-                    msg_list = ','.join(self.msg_ids)
-                    for label in self.labels:
-                        code, response = account.copy(msg_list, label)
-                        if not code == 'OK':
-                            break
-                for msg_id in self.msg_ids:
-                    forward(account, msg_id, smtp, self.forwardees)
-            if not code == 'OK':
-                self.error = response[0]
-        except imaplib.IMAP4.error as e:
-            self.error = e
-
 
 def fetch_message(account, msg_id):
     code, msg_data = account.fetch(msg_id, '(RFC822)')
@@ -99,23 +20,6 @@ def fetch_message(account, msg_id):
         if isinstance(response_part, tuple):
             return email.message_from_string(response_part[1])
     return None
-
-
-def forward(account, msg_id, smtp, recipients):
-    if not smtp:
-        return
-    msg = fetch_message(account, msg_id)
-    if msg:
-        for recipient in recipients:
-            sender = 'ryan.b.shaw@gmail.com'
-            subject = msg.get('Subject', '')
-            if DEBUG:
-                print >> sys.stderr, \
-                    'Forwarding "%s" to <%s>' % (subject, recipient)
-            msg.replace_header('To', recipient)
-            msg.replace_header('Subject', 'Fwd: %s' % subject)
-            del msg['CC']
-            smtp.sendmail(sender, recipient, msg.as_string())
 
 
 def movemail(from_account, to_account):
@@ -142,33 +46,6 @@ def close(accounts):
         a.logout()
 
 
-def load_filters(filename):
-    tree = etree.parse(filename)
-    return [Filter(entry) for entry in tree.findall('{%s}entry' % ATOM_NS)]
-
-
-def run_filters(account, filters, smtp=None, expunge=True):
-    account.select('INBOX')
-    to_delete = set()
-    for f in filters:
-        f.run(account, smtp)
-        msg = None
-        if f.error is None:
-            if len(f.msg_ids) > 0:
-                msg = '%s\nFiltered %s messages.' % (
-                    ' '.join(f.criteria), len(f.msg_ids))
-                if f.archive:
-                    to_delete.update(f.msg_ids)
-        else:
-            msg = 'Problem running filter %s: %s' % (f.id, f.error)
-        if DEBUG and msg is not None:
-            print >> sys.stderr, msg
-    if expunge and len(to_delete) > 0:
-        msg_list = ','.join(to_delete)
-        code, response = account.store(msg_list, '+FLAGS', r'(\Deleted)')
-        if code == 'OK':
-            account.expunge()
-
 frombox = imaplib.IMAP4_SSL(FROM_IMAP)
 frombox.login(FROM_USER, FROM_PASSWORD)
 
@@ -182,11 +59,7 @@ if DEBUG:
     tobox_smtp.set_debuglevel(1)
 
 try:
-    filters = load_filters(
-        os.path.join(os.path.dirname(__file__), 'mailFilters.xml'))
-    run_filters(frombox, filters, smtp=tobox_smtp, expunge=False)
     movemail(frombox, tobox)
-    run_filters(tobox, filters)
 finally:
     close([frombox, tobox])
     tobox_smtp.quit()
